7c3efced4b78822be603e39564c10effadb59820
萧凡宇 <703463228@qq.com>
2016-08-31 17:31:33 +0800

    Initial Commit

---
+++ SWAttributedLabel/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  SWAttributedLabel
+//
+//  Created by mxc235 on 16/8/31.
+//  Copyright © 2016年 mxc235. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
